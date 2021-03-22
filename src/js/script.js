183ac5b9e274561403c771bb7e940b32435ef46f
Marta Bruczko <marta.bruczko@gmail.com>
2021-03-22 18:52:51 +0100

    Add initAction

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,9 @@
     const select = {
         books: '#template-book',
         booksList: '.books-list',
+        bookImage: '.book__image',
+        bookId: '.data-id',
+        favoriteBook: '.favorite',
     };
 
     const templates = {
@@ -15,23 +18,51 @@
 
 
     function render() {
+
         for (let elem of dataSource.books) {
-            const generatedHTML = templates.books(dataSource.books);
-            console.log(generatedHTML);
+            const generatedHTML = templates.books(elem);
 
             const elementDOM = utils.createDOMFromHTML(generatedHTML);
-            console.log(elementDOM);
 
-            const booksContainer = document.querySelector(select.booksList);
+            const booksList = document.querySelector(select.booksList);
 
-            booksContainer.appendChild(elementDOM);
+            booksList.appendChild(elementDOM);
 
         }
     }
 
     render();
 
+    const favoriteBooks = [];
+    console.log(favoriteBooks);
 
+    function initActions() {
+
+        const bookImage = document.querySelectorAll(select.bookImage);
+        
+
+
+
+
+        for (let img of bookImage) {
+            img.addEventListener('dblclick', function (event) {
+                event.preventDefault();
+                const favoriteBook = document.querySelector(select.favoriteBook);
+                console.log(favoriteBook);
+                if (img == favoriteBook) {
+                    img.classList.remove('favorite');
+                    const index = favoriteBooks.indexOf();
+                } else {
+                    img.classList.add('favorite');
+                    const bookId = img.getAttribute('data-id');
+                    favoriteBooks.push(bookId);
+                }
+
+            });
+        }
+    }
+
+    initActions();
 }
 
 
